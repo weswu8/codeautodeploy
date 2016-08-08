e96dae75cf03edc205a5d6ec694f86616e58a6a0
wuwesley <wuwesley@WJ>
2016-08-08 20:17:49 +0800

    edit server.sh

--- service.java.sh
+++ service.java.sh
@@ -4,21 +4,22 @@
 #       http://www.jcgonzalez.com/linux-java-service-wrapper-example
 ####
 # step 1: sudo vi /etc/init.d/mytestserv
-# step 2: modify the SERVICE_NAME, PATH_TO_JAR, and choose a PID_PATH_NAME 
+# step 2: modify the SERVICE_NAME, PATH_TO_JAR, JAR_NAME,and choose a PID_PATH_NAME 
 # step 3: sudo chmod +x /etc/init.d/mytestserv
 # step 4: sudo service mytestserv start/stop/restart
 # step 5: sudo update-rc.d mytestserv defaults
 ### END INIT INFO
 SERVICE_NAME=eurekaserver
-PATH_TO_JAR=/usr/local/eurekaserver/EurekaServer-0.0.1-SNAPSHOT.jar
+PATH_TO_JAR=/usr/local/eurekaserver
+JAR_NAME=EurekaServer-0.0.1-SNAPSHOT.jar
 PID_PATH_NAME=/tmp/eurekaserver-pid
+JAVA_HOME=/usr/java/jdk1.8.0_101
 #### END CONFIGURATION
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup $JAVA_HOME/bin/java -jar $PATH_TO_JAR/$JAR_NAME $PATH_TO_JAR 2 >> /dev/null >> /dev/null & echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
@@ -43,8 +44,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup $JAVA_HOME/bin/java -jar $PATH_TO_JAR $PATH_TO_JAR 2 >> /dev/null >> /dev/null & echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
