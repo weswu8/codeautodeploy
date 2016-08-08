9af26fae6fbed686c07c70c5722a7d055954b74a
wuwesley <wuwesley@WJ>
2016-08-08 16:33:06 +0800

    edit server.sh

--- service.python.sh
+++ service.python.sh
@@ -4,20 +4,20 @@
 #       http://www.jcgonzalez.com/linux-java-service-wrapper-example
 ####
 # step 1: sudo vi /etc/init.d/mytestserv
-# step 2: modify the SERVICE_NAME, PATH_TO_JAR, and choose a PID_PATH_NAME 
+# step 2: modify the SERVICE_NAME, PATH_TO_PY, and choose a PID_PATH_NAME 
 # step 3: sudo chmod +x /etc/init.d/mytestserv
 # step 4: sudo service mytestserv start/stop/restart
 # step 5: sudo update-rc.d mytestserv defaults
 ### END INIT INFO
-SERVICE_NAME=eurekaserver
-PATH_TO_JAR=/usr/local/eurekaserver/EurekaServer-0.0.1-SNAPSHOT.jar
-PID_PATH_NAME=/tmp/eurekaserver-pid
+SERVICE_NAME=codeautodeploy
+PATH_TO_PY=/usr/local/codeautodeploy/codeautodeploy.py
+PID_PATH_NAME=/tmp/codeautodeploy-pid
 #### END CONFIGURATION
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            python $PATH_TO_PY /tmp 2>> /dev/null >> /dev/null &
                         echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
@@ -43,7 +43,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar $PATH_TO_PY /tmp 2>> /dev/null >> /dev/null &
                         echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
