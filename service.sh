077e318c89d259eebb7c609e4ea2585bb596f700
wuwesley <wuwesley@WJ>
2016-08-08 16:13:17 +0800

    edit server.sh

--- service.sh
+++ service.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+#### BEGIN INIT INFO
+# From: https://www.topjavablogs.com/news/how-to-make-spring-boot-application-start-when-linux-boots
+#       http://www.jcgonzalez.com/linux-java-service-wrapper-example
+####
+# step 1: sudo vi /etc/init.d/mytestserv
+# step 2: modify the SERVICE_NAME, PATH_TO_JAR, and choose a PID_PATH_NAME 
+# step 3: sudo chmod +x /etc/init.d/mytestserv
+# step 4: sudo service mytestserv start/stop/restart
+### END INIT INFO
 SERVICE_NAME=eurekaserver
 PATH_TO_JAR=/usr/local/eurekaserver/EurekaServer-0.0.1-SNAPSHOT.jar
 PID_PATH_NAME=/tmp/eurekaserver-pid
+#### END CONFIGURATION
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
